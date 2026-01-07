5baa4259237ec7e23ebd68c1499563c4a57fe792
Azhan <127591140+AZHANAMEEN@users.noreply.github.com>
2026-01-07 14:39:19 +0530

    SOME SPRITE CCHANGES

--- home.gd
+++ home.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	lvl.position.x += -1
+	lvl.position.x += -2
