0e411940d6b4f219c38defff4fa811ec09a6cf15
Azhan <127591140+AZHANAMEEN@users.noreply.github.com>
2026-01-07 20:50:52 +0530

    got some more stuff done

---
+++ autoloadscript.gd
@@ -0,0 +1,11 @@
+extends Node
+var prs1 = false 
+var prs2 = false
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
