296c05f9634c1b837167424f08f55d21a1bf2e11
Azhan <127591140+AZHANAMEEN@users.noreply.github.com>
2026-07-02 19:10:21 +0530

    nill

--- scenes/homescreen.gd
+++ scenes/homescreen.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/home.tscn")
+
+
+func _on_touch_screen_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/shop.tscn")
